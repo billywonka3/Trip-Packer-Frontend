4713e3128fcfae7eef1bfbb3453d39ac5e9346cb
billywonka3 <billywonka3@gmail.com>
2019-07-31 12:38:21 -0400

    react user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Link, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from './components/Signup.js';
+import Login from './components/Login.js';
+import AuthService from './services/AuthService.js';
+
+import Navbar from './components/Navbar.js'
+
+import axios from 'axios';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { 
+      listOfProjects: [],
+      currentlyLoggedIn: null,
+      ready: false,
+      signupShowing: false,
+      loginShowing: false,
+    };
+
+    this.service = new AuthService();
+  }
+
+  // getAllProjects = () => {
+  //   axios.get(`http://localhost:3000/api/projects`, {withCredentials: true})
+  //   .then(responseFromApi => {
+  //     this.setState({
+  //       listOfProjects: responseFromApi.data, ready: true
+  //     })
+  //   })
+  // }
+
+  getCurrentlyLoggedInUser = () =>{
+    this.service.currentUser()
+    .then((theUser)=>{
+      this.setState({currentlyLoggedIn: theUser})
+    })
+    .catch(()=>{
+      this.setState({currentlyLoggedIn: null})
+    })
+  }
+
+  toggleForm = (whichForm) =>{
+    let theForm;
+
+    if(whichForm === "signup"){
+      theForm = 'signupShowing'
+    } else {
+      theForm = 'loginShowing'
+    }
+
+    this.setState({[theForm]: !this.state[theForm]})
+  }
+
+  componentDidMount() {
+      this.getAllProjects();
+      this.getCurrentlyLoggedInUser();
+  }
+
+  render(){
+    console.log('=-=-=-=-=-=-=-',this.state);
+
+    return (
+      <div>
+        <Navbar 
+        theUser = {this.state.currentlyLoggedIn} 
+        pleaseLogOut = {()=> this.service.logout()}
+        toggleForm = {this.toggleForm}
+        getUser = {this.getCurrentlyLoggedInUser}
+        />
+
+        {this.state.signupShowing && 
+          <Signup getUser = {this.getCurrentlyLoggedInUser}
+          toggleForm = {this.toggleForm}
+          />
+        }
+
+        {this.state.loginShowing && 
+          <Login getUser = {this.getCurrentlyLoggedInUser}
+          toggleForm = {this.toggleForm}
+          />
+        }
+
+        <Switch>
+          <Route exact path="/projects" render ={(props)=> <ProjectIndex
+          {...props} 
+          theUser = {this.state.currentlyLoggedIn} 
+          allTheProjects ={this.state.listOfProjects}
+          getData = {this.getAllProjects}
+          ready = {this.state.ready}
+          theUser = {this.state.currentlyLoggedIn}
+          />} />
+
+          <Route exact path="/projects/:theID" render ={(props)=> <ProjectDetails
+          {...props} 
+          allTheProjects ={this.state.listOfProjects}
+          ready = {this.state.ready}
+          getData = {this.getAllProjects}
+          theUser = {this.state.currentlyLoggedIn}
+          />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/services/AuthService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+class AuthService {
+  constructor() {
+    let service = axios.create({
+      baseURL: 'http://localhost:3000/api/auth',
+      withCredentials: true
+    });
+    this.service = service;
+  }
+
+  signup = (username, password) => {
+    return this.service.post('/signup', {username:username, password:password})
+    .then(response => response.data)
+  }
+
+  login = (username, password) =>{
+    return this.service.post('/login', {username, password})
+    .then(response => response.data)
+  }
+
+  currentUser = () =>{
+    return this.service.get('/getcurrentuser')
+    .then(response => response.data)
+  }
+
+  logout = () =>{
+    return this.service.post('/logout', {})
+    .then(response => response.data)
+  }
+}
+
+export default AuthService;
