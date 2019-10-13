7d15d7165435c4d8e738996ca70798ab0fc5af3f
billywonka3 <billywonka3@gmail.com>
2019-10-13 16:52:43 -0400

    10-13

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,14 @@ function Home(props){
                 <div>
                     <div className= "landing-page">
                         <div className= "landing-text">
-                            <h2> Travelling is about experiencing new things, and being unprepared can mean missed opportunities. </h2>
-                            <h2> Trip-Packer helps you generate customizable packing checklists to make sure you don’t miss a thing. </h2>
+                            <h1> Trip-Packer </h1>
+                            <h4> Customiazable Lists for Packing your Bags & Prepping your Home </h4>
                         </div>
                         {/* <div >
                             <img src="/images/laoTzu-miles.jpg"/>
                         </div> */}
                     </div>
+                    
                     <div className="img-stripe">
                         <img src="/images/backgrounds/maldives.jpeg"/>
                         <img src="/images/backgrounds/kyoto-yakasa-pagoda.jpg"/>
